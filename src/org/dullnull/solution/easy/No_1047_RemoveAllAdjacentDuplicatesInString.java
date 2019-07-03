48138ec64c2e09d9d434427f2ad7902aa4f50cd7
yuesheng <iyuesheng@hotmail.com>
2019-07-04 01:03:55 +0800

    Add 1047 and 1051 problem solution

---
+++ src/org/dullnull/solution/easy/No_1047_RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,30 @@
+package org.dullnull.solution.easy;
+
+import java.util.Stack;
+
+/**
+ * @author iisheng
+ * @date 2019/07/01 22:38:13
+ */
+public class No_1047_RemoveAllAdjacentDuplicatesInString {
+
+    public static String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Character c : stack) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates("abbaca"));
+    }
+}
---
+++ src/org/dullnull/solution/easy/No_1051_HeightChecker.java
@@ -0,0 +1,28 @@
+package org.dullnull.solution.easy;
+
+import java.util.Arrays;
+
+/**
+ * @author iisheng
+ * @date 2019/07/03 23:46:07
+ */
+public class No_1051_HeightChecker {
+    public static void main(String[] args) {
+        System.out.println(heightChecker(new int[]{1, 1, 4, 2, 1, 3}));
+    }
+
+
+    public static int heightChecker(int[] heights) {
+        // [1,1,4,2,1,3]
+        // [1,1,1,2,3,4]
+        int[] arr = heights.clone();
+        Arrays.sort(arr);
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != heights[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
