28617c23e70db8ce2b2b38710975b53569ba16dc
yuesheng <iyuesheng@hotmail.com>
2019-08-10 15:29:27 +0800

    Add 0236 and 0849 problem solution

---
+++ src/org/dullnull/solution/easy/No_0849_MaximizeDistanceToClosestPerson.java
@@ -0,0 +1,40 @@
+package org.dullnull.solution.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author iisheng
+ * @date 2019/08/07 15:38:18
+ */
+public class No_0849_MaximizeDistanceToClosestPerson {
+
+    public static int maxDistToClosest(int[] seats) {
+        int max = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < seats.length; i++) {
+            if (seats[i] == 1) {
+                list.add(i);
+            }
+            int l = list.size();
+            if (l >= 2 && i >= 1) {
+                int tmp = list.get(l - 1) - list.get(l - 2);
+                if (tmp > max) {
+                    max = tmp;
+                }
+            }
+        }
+        max /= 2;
+        if (seats[0] == 0) {
+            max = Math.max(max, list.get(0) - 0);
+        }
+        if (seats[seats.length - 1] == 0) {
+            max = Math.max(max, seats.length - 1 - list.get(list.size() - 1));
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxDistToClosest(new int[]{0, 0, 0, 1}));
+    }
+}
---
+++ src/org/dullnull/solution/medium/No_0236_LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,35 @@
+package org.dullnull.solution.medium;
+
+/**
+ * @author iisheng
+ * @date 2019/08/08 17:30:37
+ */
+public class No_0236_LowestCommonAncestorOfABinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+
+        return root;
+    }
+}
