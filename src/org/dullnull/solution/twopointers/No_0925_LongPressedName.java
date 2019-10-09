ab2ada36c983dec2be5b4c5ed807be597c31609c
yuesheng <iyuesheng@hotmail.com>
2019-10-09 20:26:06 +0800

    Add 0532 and 0925 problem solution

---
+++ src/org/dullnull/solution/twopointers/No_0925_LongPressedName.java
@@ -0,0 +1,37 @@
+package org.dullnull.solution.twopointers;
+
+/**
+ * @author iisheng
+ * @date 2019/10/09 18:43:02
+ */
+public class No_0925_LongPressedName {
+
+    public static boolean isLongPressedName(String name, String typed) {
+
+        int slow = 0;
+        int fast = 0;
+
+        while (slow < name.length() && fast < typed.length()) {
+            if (name.charAt(slow) == typed.charAt(fast)) {
+                slow++;
+                fast++;
+            } else {
+                fast++;
+            }
+        }
+
+        if (slow == name.length()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isLongPressedName("vtkgn", "vttkgnn"));
+        System.out.println(isLongPressedName("kikcxmvzi", "kiikcxxmmvvzz"));
+        System.out.println(isLongPressedName("alex", "aaleex"));
+        System.out.println(isLongPressedName("saeed", "ssaaedd"));
+        System.out.println(isLongPressedName("leelee", "lleeelee"));
+    }
+}
