f932c464a532bb7cf427d4bb62ca61f31e0b2361
yuesheng <iyuesheng@hotmail.com>
2019-10-17 01:01:16 +0800

    Add 0096 Unique Binary Search Trees

---
+++ src/org/dullnull/solution/dp/No_0096_UniqueBinarySearchTrees.java
@@ -0,0 +1,28 @@
+package org.dullnull.solution.dp;
+
+/**
+ * @author iisheng
+ * @date 2019/10/13 19:59:45
+ */
+public class No_0096_UniqueBinarySearchTrees {
+
+    public static int numTrees(int n) {
+        return (int) CatalanNumber(n);
+    }
+
+    public static double CatalanNumber(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return CatalanNumber(n - 1) * 2 * (2 * n - 1) / (n + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numTrees(2));
+        System.out.println(numTrees(4));
+        System.out.println(numTrees(5));
+        System.out.println(numTrees(19));
+
+    }
+}
