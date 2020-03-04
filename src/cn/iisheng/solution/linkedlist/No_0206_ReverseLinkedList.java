b48e794aa7e60a84f582d0cc038b1380bebcdea3
yuesheng <iyuesheng@hotmail.com>
2020-03-04 22:18:35 +0800

    Review 0206 Reverse Linked List

--- src/cn/iisheng/solution/linkedlist/No_0206_ReverseLinkedList.java
+++ src/cn/iisheng/solution/linkedlist/No_0206_ReverseLinkedList.java
@@ -10,25 +10,56 @@ import cn.iisheng.solution.utils.ListUtils;
 public class No_0206_ReverseLinkedList {
 
     public static ListNode reverseList(ListNode head) {
-        // 定义一个新 头结点 代表反转后的链表的头结点
+        // 整体思路
+        // 定义一个 新的头节点
+        // 遍历 原链表 一个一个 加到新的头节点的前面 即可 实现 反转
+
+        // 定义一个新的头节点
         ListNode newHead = null;
+
+        // 从头到尾遍历原链表 所有节点
         while (head != null) {
-            // 定义一个结点变量 代表 head 的next结点
-            ListNode next = head.next;
-            // 将head 的next 指向 新的头结点
+            // 定义一个临时节点 存放 head 节点 之后的 链表数据
+            ListNode temp = head.next;
+            // 将当前 节点（原链表遍历到的一个节点） 放到 新的头节点的前面
             head.next = newHead;
-            // 将新头结点 指向 head  相当于将 一个个遍历的结点 一个个 插入 新链表的头结点上
+            // 将head 节点 赋值 给 newHead，newHead节点做反转后的头节点
             newHead = head;
-            // 将head 指向下一个 结点
-            head = next;
+            // 将temp 链表 赋值给 head 继续完成遍历
+            head = temp;
         }
         return newHead;
     }
 
+    public static ListNode reverseList1(ListNode head) {
+        // 递归写法 基于循环写法的改变
+        return recursion(head, null);
+    }
+
+    public static ListNode recursion(ListNode head, ListNode newHead) {
+        // 相当于循环到原链表的最后一个节点
+        if (head == null) {
+            return newHead;
+        } else {
+            // 相当于 while 循环内部的逻辑
+
+            // 定义一个临时节点 存放 head 节点 之后的 链表数据
+            ListNode temp = head.next;
+            // 将当前 节点（原链表递归到的一个节点） 放到 新的头节点的前面
+            head.next = newHead;
+            // 将head 节点 赋值 给 newHead，newHead节点做反转后的头节点
+            newHead = head;
+            // 将temp 链表 赋值给 head 继续完成递归
+            head = temp;
+            return recursion(head, newHead);
+        }
+    }
+
+
     public static void main(String[] args) {
         ListNode head = ListUtils.createLinkedList(new Integer[]{1, 2, 3, 4, 5, null});
         ListUtils.print(head);
-        ListNode reverseList = reverseList(head);
+        ListNode reverseList = reverseList1(head);
         ListUtils.print(reverseList);
     }
 }
