fd0f0955a41ef4d0fd3b1ce5b0d0b22e670f4b20
yuesheng <iyuesheng@hotmail.com>
2020-03-22 15:06:04 +0800

    Reformat code

--- src/cn/iisheng/solution/array/No_1109_CorporateFlightBookings.java
+++ src/cn/iisheng/solution/array/No_1109_CorporateFlightBookings.java
@@ -8,34 +8,43 @@ import java.util.Arrays;
  */
 public class No_1109_CorporateFlightBookings {
 
-    public static int[] corpFlightBookings(int[][] bookings, int n) {
-        // 初始化一个结果数据
-        int[] result = new int[n];
-        // 遍历预订记录
-        for (int[] booking : bookings) {
-            // ① 当遍历到一个预定记录 [i,j,k]的时候
-            // ② result[i-1] 要 加 k
-            result[booking[0] - 1] += booking[2];
-            // ③ result[j+1] 要 减 k (j+1)<n 不然数组越界
-            if (booking[1] < n) {
-                result[booking[1]] -= booking[2];
+    public static int[] corpFlightBookings(
+            int[][] bookings, int n) {
+
+        int[] answer = new int[n];
+
+        // 遍历bookings 计算航班i+1 对航班i 变化的预订数
+        for (int[] b : bookings) {
+            // 增加的预订数
+            answer[b[0] - 1] += b[2];
+            // 防止数组越界
+            if (b[1] < n) {
+                // 减少的预订数量
+                answer[b[1]] -= b[2];
             }
         }
 
-        for (int index = 1; index < n; index++) {
-            // 比如 在 预定记录 [i,j,k]的时候
-
-            // 如果 index = i + 1, 并且 index < j + 1
-            // 因为result[i] 已经 加过 k （从0变成k）
-            // result[i+1] = result[i] + result[i+1] = k + 0 =k
-
-            // 如果 index = j + 1
-            // 因为result[j] 已经 加过 k（通过 result[index] += result[index - 1]）
-            // 又因为 result[j+1] 已经 减了 k 通过③
-            // 所以 result[j+1] = result[j] + result[j+1] = k + (-k) = 0
-            result[index] += result[index - 1];
+        // 航班i的预订数等于,i-1的预订数，加i时刻变化的预定数
+        for (int i = 1; i < n; i++) {
+            answer[i] += answer[i - 1];
         }
-        return result;
+        return answer;
+    }
+
+    public int[] corpFlightBookings3(
+            int[][] bookings, int n) {
+
+        int[] answer = new int[n];
+
+        // 遍历整个bookings数组
+        for (int[] b : bookings) {
+            // 内层循环把每个航班预订数加上
+            for (int i = b[0] - 1;
+                 i <= b[1] - 1; i++) {
+                answer[i] += b[2];
+            }
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
--- src/cn/iisheng/solution/greedy/No_1094_CarPooling.java
+++ src/cn/iisheng/solution/greedy/No_1094_CarPooling.java
@@ -1,6 +1,5 @@
 package cn.iisheng.solution.greedy;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -11,66 +10,31 @@ import java.util.TreeMap;
 public class No_1094_CarPooling {
 
     public static void main(String[] args) {
-        System.out.println(carPooling2(new int[][]{{2, 1, 5}, {3, 3, 7}}, 4));
+        System.out.println(carPooling(new int[][]{{2, 1, 5}, {3, 3, 7}}, 4));
     }
 
-    public static boolean carPooling(int[][] trips, int capacity) {
-        int[] result = new int[1000];
-        for (int[] trip : trips) {
-            result[trip[1]] += trip[0];
-            if (trip[2] + 1 < 1000) {
-                result[trip[2]] -= trip[0];
-            }
+    public boolean carPooling1(
+            int[][] trips, int capacity) {
+        // 使用TreeMap 是为了让元素根据key排序
+        TreeMap<Integer, Integer> map =
+                new TreeMap<>();
+        for (int[] t : trips) {
+            int v = map.getOrDefault(t[1], 0) + t[0];
+            // 上车乘客数量
+            map.put(t[1], v);
+            v = map.getOrDefault(t[2], 0) - t[0];
+            // 下车乘客数量
+            map.put(t[2], v);
         }
 
-        for (int i = 1; i < 1000; i++) {
-            result[i] += result[i - 1];
-            if (result[i] > capacity) {
-                return false;
-            }
-        }
-        return true;
-
-    }
-
-    public static boolean carPooling1(int[][] trips, int capacity) {
-        Map<Integer, Integer> start = new HashMap<>();
-        Map<Integer, Integer> end = new HashMap<>();
-        int max = trips[0][2], min = trips[0][1];
-        for (int[] trip : trips) {
-            start.put(trip[1], start.getOrDefault(trip[1], 0) + trip[0]);
-            end.put(trip[2], end.getOrDefault(trip[2], 0) + trip[0]);
-            min = Math.min(min, trip[1]);
-            max = Math.max(max, trip[2]);
-        }
         int cur = 0;
 
-        for (int i = min; i <= max; i++) {
-            if (end.containsKey(i)) {
-                cur -= end.get(i);
-            }
-            if (start.containsKey(i)) {
-                cur += start.get(i);
-            }
-            if (cur > capacity) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean carPooling2(int[][] trips, int capacity) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for (int[] trip : trips) {
-            map.put(trip[1], map.getOrDefault(trip[1], 0) + trip[0]);
-            map.put(trip[2], map.getOrDefault(trip[2], 0) - trip[0]);
-        }
-        int cur = 0;
-
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry
+                : map.entrySet()) {
             Integer value = entry.getValue();
+            // 当前数量=之前数量+变化的数量
             cur += value;
+
             if (cur > capacity) {
                 return false;
             }
@@ -78,40 +42,27 @@ public class No_1094_CarPooling {
         return true;
     }
 
-    public static boolean carPooling3(int[][] trips, int capacity) {
-        int[] result = new int[1000 + 1];
-        for (int[] trip : trips) {
-            result[trip[1]] += trip[0];
-            result[trip[2]] -= trip[0];
+    public  static  boolean carPooling(
+            int[][] trips, int capacity) {
+
+        // 最远行程 数组长度
+        int max = 0;
+        for (int[] t : trips) {
+            max = Math.max(max, t[2]);
         }
 
-        int currCapacity = 0;
-        for (int i = 0; i < result.length; ++i) {
-            currCapacity += result[i];
-            if (currCapacity > capacity) {
-                return false;
-            }
-        }
-        return true;
-
-    }
-
-    public static boolean carPooling4(int[][] trips, int capacity) {
-        int maxDest = 0;
-        for (int[] trip : trips) {
-            maxDest = Math.max(maxDest, trip[2]);
+        // 所有要乘车的乘客数量
+        int[] passengers = new int[max + 1];
+        for (int[] t : trips) {
+            passengers[t[1]] += t[0];
+            passengers[t[2]] -= t[0];
         }
 
-        int[] costs = new int[maxDest + 1];
-        for (int[] trip : trips) {
-            costs[trip[1]] += trip[0];
-            costs[trip[2]] -= trip[0];
-        }
-
-        int passengersCount = 0;
-        for (int cost : costs) {
-            passengersCount += cost;
-            if (passengersCount > capacity) {
+        int cur = 0;
+        for (int passenger : passengers) {
+            // 当前数量 = 之前数量 + 变化的数量
+            cur += passenger;
+            if (cur > capacity) {
                 return false;
             }
         }
